7320550db8494c50f52c4f3a57d7f00bd8f893c1
holewinski <szymon.holewinski@gmail.com>
2020-02-08 12:34:27 +0100

    Error message for buttons

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,9 @@
 const hamburger = document.getElementById('hamburger'),
     menu = document.getElementById('menu'),
     banner = document.getElementById('banner'),
-    formSelect = [...document.getElementsByClassName('form__select')];
-
+    formSelect = [...document.getElementsByClassName('form__select')],
+    form = document.getElementById('form'),
+    error = document.getElementById('error-message');
 //Declaration of functions
 
 function changeBanner() {
@@ -26,13 +27,21 @@ function setClass(element, className) {
     const list = element.classList;
     list.contains(className) ? list.remove(className) : list.add(className);
 }
+function showError() {
+    setClass(error, 'visible');
+}
+
+//Calling functions
 formSelect.forEach(element => {
+
     const select = element.getElementsByTagName('select')[0],
         options = [...element.getElementsByTagName('option')],
         optionsContainer = document.createElement('div'),
         optionElement = document.createElement('div');
+
     optionsContainer.className = 'form__options';
     optionElement.className = 'form__option form__input';
+
     options.forEach((option, index) => {
 
         const optionNode = optionElement.cloneNode(true);
@@ -46,8 +55,14 @@ formSelect.forEach(element => {
     })
     element.appendChild(optionsContainer);
 });
-
-//Calling functions
 changeBanner();
 hamburger.onclick = () => setClass(menu, 'show');
 window.onclick = resize = () => changeBanner();
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    showError();
+})
+error.addEventListener('click', e => {
+    e.target != error.firstElementChild ? showError() : null;
+})
+
